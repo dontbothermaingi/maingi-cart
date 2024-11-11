65a097503ad2ca300f888a71159a54c503a9838b
Denis <denismaingi49@gmail.com>
2024-11-11 21:36:46 +0300

    Cart 5

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -90,7 +90,7 @@ function Dashboard() {
                                         boxShadow: '0 8px 30px rgba(0,0,0,0.15)',
                                     },
                                     '@media (max-width:600px)': {
-                                        height: '370px',
+                                        height: '420px',
                                     },
                                 }}
                             >
@@ -175,7 +175,7 @@ function Dashboard() {
                                         boxShadow: '0 8px 30px rgba(0,0,0,0.15)',
                                     },
                                     '@media (max-width:600px)': {
-                                        height: '400px',
+                                        height: '450px',
                                     },
                                 }}
                             >
